ab308e3dcebb6b39d498e7dd2de831223c9a747a
Mark <lathon2018.com>
2025-08-07 14:08:37 -0400

    Added Cartitems components code

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,30 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const{name,image,cost} = action.payload;
+
+      //check if the item already exists
+      const existingItem = state.items.find(item => item.name === name);
+      if(existingItem)
+      {
+        existingItem.quantity++;
+      }
+      else
+      {
+        state.items.push({name,image,cost,quantity: 1});
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+      const{name,cost} = action.payload;
 
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if(itemToUpdate)
+      {
+        itemToUpdate.quantity = quantity;
+      }
     
     },
   },
